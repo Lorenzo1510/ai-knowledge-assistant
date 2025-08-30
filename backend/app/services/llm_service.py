4cc38d014ae3715da89169c3d19adae10caaf1af
Lorenzo Catanzaro <lorenzo.c15@hotmail.it>
2025-08-30 19:07:41 +0200

    test di LLM

--- backend/app/services/llm_service.py
+++ backend/app/services/llm_service.py
@@ -1,8 +1,11 @@
-from langchain.chat_models import ChatOpenAI
+import os
+from langchain.llms import HuggingFaceHub
 from langchain.prompts import PromptTemplate
+from langchain.llms import HuggingFaceEndpoint
+
+hg_api_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 
 def llm_predict(context: str, question: str) -> str:
-    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
     template = """
     Rispondi alla domanda in base al seguente contesto:
     {context}
@@ -11,6 +14,12 @@ def llm_predict(context: str, question: str) -> str:
     """
     prompt = PromptTemplate(template=template, input_variables=["context", "question"])
     final_prompt = prompt.format(context=context, question=question)
-    response = llm.predict(final_prompt)
-    print("AAAAAAAAAAAAAAAAAAAAAAA")
-    return response
+
+    llm = HuggingFaceEndpoint(
+        endpoint_url="https://api-inference.huggingface.co/models/google/flan-t5-large",
+        huggingfacehub_api_token=hg_api_token,
+        temperature=0.7,
+        max_length=512
+    )
+    
+    return llm.predict(final_prompt)
